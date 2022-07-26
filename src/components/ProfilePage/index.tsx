79e1325db280cb66ba1b171e7355d78ed95622fe
Natan xavier <natanxavierr457@gmail.com>
2022-07-26 13:15:38 -0300

    twitter

---
+++ src/components/ProfilePage/index.tsx
@@ -0,0 +1,59 @@
+import React from 'react';
+
+import Feed from '../Feed';
+
+import {
+  Container,
+  Banner,
+  Avatar,
+  ProfileData,
+  LocationIcon,
+  CakeIcon,
+  Followage,
+  EditButton,
+} from './styles';
+
+const ProfilePage: React.FC = () => {
+  return (
+    <Container>
+      <Banner>
+        <Avatar />
+      </Banner>
+
+      <ProfileData>
+        <EditButton outlined>Editar perfil</EditButton>
+
+        <h1>Natan Xavier</h1>
+        <h2>@NatanXavier</h2>
+
+        <p>
+          Desenvolvedor reactJS
+        </p>
+
+        <ul>
+          <li>
+            <LocationIcon />
+            Fortaleza, Brasil
+          </li>
+          <li>
+            <CakeIcon />
+            Nascido(a) em 07 de abril de 2000
+          </li>
+        </ul>
+
+        <Followage>
+          <span>
+            seguindo <strong>94</strong>
+          </span>
+          <span>
+            <strong>672 </strong> seguidores
+          </span>
+        </Followage>
+      </ProfileData>
+
+      <Feed />
+    </Container>
+  );
+};
+
+export default ProfilePage;
